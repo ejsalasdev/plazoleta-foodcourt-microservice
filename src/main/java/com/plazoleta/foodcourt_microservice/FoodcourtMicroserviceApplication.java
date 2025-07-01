d9e148453db84392b17cf2e44abe11c6ce5df393
Edgar J. Salas <ejsalasdev@gmail.com>
2025-07-01 14:12:34 -0500

    start project

---
+++ src/main/java/com/plazoleta/foodcourt_microservice/FoodcourtMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.plazoleta.foodcourt_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcourtMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcourtMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plazoleta/foodcourt_microservice/FoodcourtMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plazoleta.foodcourt_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcourtMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
